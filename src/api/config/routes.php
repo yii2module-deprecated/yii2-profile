80ec5ca00a1c39ede6aa032e7474687d7a68d310
Vitaliy <theyamshikov@yandex.ru>
2018-02-24 16:50:42 +0600

    make routes config

---
+++ src/api/config/routes.php
@@ -0,0 +1,20 @@
+<?php
+
+$version = API_VERSION_STRING;
+
+return [
+    "GET {$version}/profile" => "profile/person/view",
+    "PUT {$version}/profile" => "profile/person/update",
+
+    "GET {$version}/profile-address" => "profile/address/view",
+    "PUT {$version}/profile-address" => "profile/address/update",
+
+    "GET {$version}/profile-car" => "profile/car/view",
+    "PUT {$version}/profile-car" => "profile/car/update",
+
+    "GET {$version}/profile-qr" => "profile/qr/view",
+
+    "GET {$version}/profile-avatar" => "profile/avatar/view",
+    "POST {$version}/profile-avatar" => "profile/avatar/update",
+    "DELETE {$version}/profile-avatar" => "profile/avatar/delete",
+];
