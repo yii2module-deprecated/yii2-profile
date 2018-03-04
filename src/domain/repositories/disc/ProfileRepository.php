c38a212d766ad4d64f123d5cab80d10366df40c4
Vitaliy <theyamshikov@yandex.ru>
2018-03-04 15:17:13 +0600

    make disc repo

---
+++ src/domain/repositories/disc/ProfileRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace yii2module\profile\domain\repositories\disc;
+
+use yii2lab\domain\repositories\ActiveDiscRepository;
+
+class ProfileRepository extends ActiveDiscRepository {
+	
+	public $table = 'profile';
+	
+}
